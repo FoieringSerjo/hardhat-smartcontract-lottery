91817224f9ecae87b6f7d75fc9ff39b3d939117b
Foiering Sergey <serjofoi@gmail.com>
2023-04-15 18:53:20 +0300

    Implementing Chainlink VRF - The Request

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol"; // ---> function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal virtual;
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 // Enter the lottery (paying some amount)
 // Pick a random winner (verifiable random)
@@ -15,15 +16,29 @@ contract Raffle is VRFConsumerBaseV2 {
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
 
     /* Events */
     event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
 
     constructor(
         address verfCoordinatorV2,
-        uint256 entranceFee
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
     ) VRFConsumerBaseV2(verfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(verfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -36,11 +51,20 @@ contract Raffle is VRFConsumerBaseV2 {
         emit RaffleEnter(msg.sender);
     }
 
-    // Chainlink docks for random number (RandomWords): https://docs.chain.link/vrf/v2/subscription/examples/get-a-random-number
+    // Chainlink docks for random number contract (RandomWords): https://docs.chain.link/vrf/v2/subscription/examples/get-a-random-number
+
     function requestRandomWinner() external {
         // Request to random number
         // Once we get it, do somthing with it
         // 2 transaction process
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, // bytes32 keyHash: The gas lane key hash value, which is the maximum gas price you are willing to pay for a request in wei. It functions as an ID of the off-chain VRF job that runs in response to requests.
+            i_subscriptionId, // The subscription ID that this contract uses for funding requests.
+            REQUEST_CONFIRMATIONS, // How many confirmations the Chainlink node should wait before responding. The longer the node waits, the more secure the random value is. It must be greater than the minimumRequestBlockConfirmations limit on the coordinator contract
+            i_callbackGasLimit,
+            NUM_WORDS // How many random values to request. If you can use several random values in a single callback, you can reduce the amount of gas that you spend per random value. The total cost of the callback request depends on how your fulfillRandomWords() function processes and stores the received random values, so adjust your callbackGasLimit accordingly.
+        );
+        emit RequestedRaffleWinner(requestId);
     }
 
     function fulfillRandomWords(
